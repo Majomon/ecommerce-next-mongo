e2079420cb3a60e8a6da40e4f855568fc30d14b6
Sebatora <sebastiana.toranzo@gmail.com>
2023-09-15 13:55:33 -0300

    route update

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,1 +1,38 @@
-git
+import Product from "@/app/models/products";
+import connectMongoDb from "@/app/mongo";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  try {
+    const { id } = params;
+    await connectMongoDb();
+    const productById = await Product.findById(id);
+    return NextResponse.json(productById);
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
+}
+
+export async function PUT(request, { params }) {
+  try {
+    await connectMongoDb();
+    const { id } = params;
+    const data = await request.json();
+    await Product.findByIdAndUpdate(id, {...data});
+    const productUpdated = await Product.findById(id)
+    return NextResponse.json(productUpdated);
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = params;
+    await connectMongoDb();
+    await Product.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Product deleted" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
+}
--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,16 +1,10 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-const useStore = create((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-}))
-
-function Counter() {
-  const { count, inc } = useStore()
-  return (
-    <div>
-      <span>{count}</span>
-      <button onClick={inc}>one up</button>
-    </div>
-  )
-}
+export const useProductsStore = create((set, get) => {
+  return {
+    products: [],
+    getProducts: async () => {
+      const res = await fetch("https://localhost:3000/api/products");
+    },
+  };
+});
